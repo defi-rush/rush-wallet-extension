e536ac7535c9daac092c2f44deaf70b6342ac7c6
web3nomad.eth <me@web3nomad.eth>
2021-09-24 17:59:29 +0800

    remove CREATE_ACCOUNT code in createWallet

--- src/store/actions/createWallet.js
+++ src/store/actions/createWallet.js
@@ -2,13 +2,13 @@ import { v4 as uuidv4 } from 'uuid'
 import { encrypt } from '../../utils/crypto'
 import buildConfig from '../../build.config'
 import { accountCreator, getNextAccountColor } from '@/utils/accounts'
-import { assets as cryptoassets } from '@/utils/chains'
+// import { assets as cryptoassets } from '@/utils/chains'
 import { chains } from '@/utils/chains'
 import { shouldApplyRskLegacyDerivation } from '../utils'
 
-export const createWallet = async ({ state, commit, dispatch }, { key, mnemonic, proxyAddress, chainId = 1 }) => {
+export const createWallet = async ({ state, commit, dispatch }, { key, mnemonic, proxyAddress, chainId }) => {
   // 这里多加一步，输入助记词之后需要再加入 proxyAddress，然后和wallet 一起encrypt
-  const { networks, defaultAssets } = buildConfig
+  const { defaultAssets } = buildConfig
   let id = uuidv4()
   let wallet
   let ownerPublicKey
@@ -28,35 +28,8 @@ export const createWallet = async ({ state, commit, dispatch }, { key, mnemonic,
     ownerPublicKey = await dispatch('addOwnerKey', { mnemonic })
     const rskLegacyDerivation = await shouldApplyRskLegacyDerivation(state.accounts, mnemonic)
     commit('CREATE_WALLET', { keySalt, encryptedWallets, wallet, rskLegacyDerivation })
-
-    commit('ENABLE_ASSETS', { network: 'mainnet', walletId: id, assets: defaultAssets.mainnet })
-    commit('ENABLE_ASSETS', { network: 'testnet', walletId: id, assets: defaultAssets.testnet })
-
-    networks.forEach(network => {
-      const assetKeys = defaultAssets[network]
-      buildConfig.chains.forEach(async chainName => {
-        const assets = assetKeys.filter(asset => {
-          return cryptoassets[asset]?.chain === chainName
-        })
-
-        const chain = chains[chainName]
-        const _account = accountCreator(
-          {
-            walletId: id,
-            account: {
-              name: `${chain.name} 1`,
-              chain: chainName,
-              addresses: [],
-              assets,
-              balances: {},
-              type: 'default',
-              index: 0,
-              color: getNextAccountColor(chainName, 0)
-            }
-          })
-        commit('CREATE_ACCOUNT', { network, walletId: id, account: _account })
-      })
-    })
+    // commit('CREATE_ACCOUNT', { network, walletId: id, account: _account })
+    dispatch('updateProxyAddressAccount')
   }
   if (!state.activeWalletId) {
     // 如果是初始化钱包的时候，activeWalletId 是空的，则 CHANGE_ACTIVE_WALLETID
@@ -66,7 +39,7 @@ export const createWallet = async ({ state, commit, dispatch }, { key, mnemonic,
   commit('ADD_PROXY_ADDRESS', { proxyAddress, chainId, walletId: id, ownerPublicKey })
   if (+state.activeProxyAddressIndex < 0) {
     // activeProxyAddressIndex 是空的，则 SET_ACTIVE_PROXY_ADDRESS_INDEX
-    commit('SET_ACTIVE_PROXY_ADDRESS_INDEX', 0)
+    dispatch('changeActiveProxyAddressIndex', { index: 0 })
   }
 
   return wallet

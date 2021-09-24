57983c7513bf27c412c7a1a0004884c60d0b391e
web3nomad.eth <me@web3nomad.eth>
2021-09-24 18:01:10 +0800

    Update build.config.js

--- src/build.config.js
+++ src/build.config.js
@@ -1,34 +1,21 @@
 
 export default {
-  defaultAssets: {
-    mainnet: [
-      'ETH',
-      'DAI',
-      'USDC',
-      'USDT',
-      'WBTC',
-      'UNI',
-      'RBTC',
-      'SOV',
-      'BNB',
-      'NEAR',
-      'MATIC',
-      'PWETH',
-      'ARBETH',
-      'FISH'
-    ],
-    testnet: [
-      'ETH',
-      'DAI',
-      'RBTC',
-      'BNB',
-      'NEAR',
-      'SOV',
-      'MATIC',
-      'PWETH',
-      'ARBETH'
-    ]
-  },
+  defaultAssets: [
+    'ETH',
+    'DAI',
+    'USDC',
+    'USDT',
+    'WBTC',
+    'UNI',
+    'RBTC',
+    'SOV',
+    'BNB',
+    'NEAR',
+    'MATIC',
+    'PWETH',
+    'ARBETH',
+    'FISH'
+  ],
   infuraApiKey: 'da99ebc8c0964bb8bb757b6f8cc40f1f',
   exploraApis: {
     testnet: 'https://liquality.io/testnet/electrs',

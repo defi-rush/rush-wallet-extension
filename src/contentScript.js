0707b5bf0601ff6c9448b69ab98d760aad438e08
web3nomad.eth <me@web3nomad.eth>
2021-09-26 16:06:04 +0800

    get chainId and networkId from activeProxyAddress

--- src/contentScript.js
+++ src/contentScript.js
@@ -12,7 +12,14 @@ import { chains } from '@/utils/chains'
 inject(providerManager())
 
 function injectEthereum (state, chain) {
-  const network = ChainNetworks[chain][state.activeNetwork]
+  // const network = ChainNetworks[chain][state.activeNetwork]
+  const { activeProxyAddressIndex, proxyAddresses } = state
+  const activeProxyAddress = (proxyAddresses && activeProxyAddressIndex >= 0) ? proxyAddresses[activeProxyAddressIndex] : null
+  const chainId = activeProxyAddress ? activeProxyAddress.chainId : null
+  const network = {
+    networkId: chainId,
+    chainId,
+  }
   inject(ethereumProvider({
     chain,
     asset: chains[chain].nativeAsset,
@@ -23,13 +30,7 @@ function injectEthereum (state, chain) {
 chrome.storage.local.get(['rush-wallet'], (storage) => {
   const state = storage['rush-wallet']
 
-  buildConfig.chains
-    .filter(chain => {
-      return isEthereumChain(chain) || chain.toLowerCase() === 'localhost' || chain.toLowerCase() === 'rush'
-    })
-    .forEach(chain => {
-      injectEthereum(state, chain)
-    })
+  injectEthereum(state, 'rush')  // 这里前段的全局变量 只有 rush
 
   if (state.injectEthereum && state.injectEthereumChain) {
     inject(overrideEthereum(state.injectEthereumChain))

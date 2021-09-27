bb66af7116539e6ca79fba9f77e4e635400142d2
web3nomad.eth <me@web3nomad.eth>
2021-09-27 13:48:23 +0800

    add CHAINS

--- src/constants/chains.js
+++ src/constants/chains.js
@@ -29,3 +29,72 @@ export const CHAIN_ID_RPC_MAPPING = {
   '137': 'https://rpc-mainnet.maticvigil.com',
   '31337': 'http://localhost:8545'
 }
+
+
+
+export const CHAINS = [{
+  chainId: 1,
+  chainName: 'Ethereum',
+  nativeCurrency: {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18
+  },
+  rpcUrl: 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
+}, {
+  chainId: 137,
+  chainName: 'Polygon POS',
+  nativeCurrency: {
+    name: 'MATIC',
+    symbol: 'MATIC',
+    decimals: 18
+  },
+  rpcUrl: 'https://rpc-mainnet.maticvigil.com'
+}, {
+  chainId: 56,
+  chainName: 'Binance Smart Chain',
+  nativeCurrency: {
+    name: 'BNB',
+    symbol: 'BNB',
+    decimals: 18
+  },
+  rpcUrl: 'https://bsc-dataseed.binance.org'
+}, {
+  chainId: 128,
+  chainName: 'Huobi ECO Chain',
+  nativeCurrency: {
+    name: 'HT',
+    symbol: 'HT',
+    decimals: 18
+  },
+  rpcUrl: 'https://http-mainnet-node.huobichain.com'
+}, {
+  chainId: 4,
+  chainName: 'Rinkeby Test Network',
+  nativeCurrency: {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18
+  },
+  rpcUrl: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+}, {
+  forking: true,
+  chainId: 71337,
+  chainName: 'hardhat-dev.defirush.io',
+  nativeCurrency: {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18
+  },
+  rpcUrl: 'https://hardhat-dev.defirush.io',
+}, {
+  forking: true,
+  chainId: 31337,
+  chainName: 'localhost',
+  nativeCurrency: {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18
+  },
+  rpcUrl: 'http://localhost:8545',
+}]

ed1645435b0711478fad1725993b3112daa3476e
web3nomad.eth <me@web3nomad.eth>
2021-09-28 14:20:02 +0800

    Update requestPermission.js

--- src/store/actions/requestPermission.js
+++ src/store/actions/requestPermission.js
@@ -30,7 +30,7 @@ export const requestPermission = async ({ state, dispatch, commit }, { origin, d
     await dispatch('requestUnlockWallet')
     if (!state.unlockedAt) throw new Error('Wallet is locked. Unlock the wallet first.')
     if (!state.activeWalletId) throw new Error('No active wallet found. Create a wallet first.')
-
+    if (!state.activeProxyAddressAddress) throw new Error('No active proxy address found. Select a proxyAddress first.')
     let { asset, method, args } = data
 
     if (!ALLOWED.some(re => re.test(method))) throw new Error('Method not allowed')

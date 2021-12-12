955f72cfd15a1b0fdcde77663fd4888b6494ae1c
ranlix <wrapped.akira@gmail.com>
2021-12-13 03:09:18 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import store from './store'
 import router from './router'
 import ToggleButton from 'vue-js-toggle-button'
 import VTooltip from 'v-tooltip'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 import '@/assets/scss/style.scss'
 
+Vue.use(ElementUI)
 Vue.use(ToggleButton)
 Vue.use(VTooltip)
 Vue.config.productionTip = false
